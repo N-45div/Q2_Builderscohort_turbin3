cbc35ae00437fb7f0b9beb9e662175285860ac24
N DIVIJ <ndivij2004@gmail.com>
2025-05-02 12:03:40 +0000

    escrow program

---
+++ escrow/programs/escrow/src/instructions/mod.rs
@@ -0,0 +1,10 @@
+#![allow(unused_imports)]
+
+pub mod make;
+pub mod take;
+
+pub use make::*;
+pub use take::*;
+
+pub mod refund;
+pub use refund::*;
---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,38 @@
+#![allow(unexpected_cfgs)]
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+use self::instructions::*;
+
+declare_id!("GNzY7rdpdwb8yywh4f6Wyp9d8YxPmqrjYDzUV6Abehru");
+
+
+#[program]
+pub mod escrow {
+    use super::*; // this brings everthing from the parent scope into the escrow mod scope
+
+    pub fn make(ctx: Context<Make>, seed: u64, receive_amount: u64) -> Result<()> {
+        ctx.accounts.make(seed, receive_amount, &ctx.bumps)?;
+        ctx.accounts.deposit(receive_amount)? ;
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund()?;
+        ctx.accounts.close()?;
+        Ok(())
+    }
+
+    // taker wants to swap token b for token a
+    // you do not have to store them in a vault like you did for token a
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.release()?;
+        ctx.accounts.close()?;
+        Ok(())
+    }
+}
+
+
+
